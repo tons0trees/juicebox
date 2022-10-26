c99587d01235b695d57b6fe28e48981766342002
tons0trees <forrest@fmcclure.com>
2022-10-26 09:57:29 -0600

    20min driver change

---
+++ db/index.js
@@ -0,0 +1,5 @@
+const {Client} = require('pg');
+
+const client = new Client('postgres://localhost:5432/juicebox-dev');
+
+module.exports = {client,}
---
+++ db/seed.js
@@ -0,0 +1,17 @@
+const {client} = require('./index');
+
+async function testDB() {
+    try {
+        client.connect();
+
+        const result = await client.query('SELECT * FROM users;');
+
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        client.end();
+    }
+}
+
+testDB();
