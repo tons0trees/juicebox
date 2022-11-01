49f145c23df2f7b789f614026de3d505525f325f
Leonardo <cruzleonardo831@gmail.com>
2022-11-01 13:15:55 -0500

    Update post working

--- api/posts.js
+++ api/posts.js
@@ -7,7 +7,7 @@ postsRouter.use((req, res, next) => {
 });
 
 const {requireUser} = require('./utils')
-const {getALLPosts, createPost}= require('../db')
+const {getALLPosts, createPost, getPostById, updatePost}= require('../db')
 
 postsRouter.post('/', requireUser, async (req, res, next) => {
     const {title, content, tags=""} = req.body
@@ -28,6 +28,40 @@ postsRouter.post('/', requireUser, async (req, res, next) => {
     } catch ({name, message}) {
         next({name, message})
     }
+})
+
+postsRouter.patch('/:postId', requireUser, async (req, res, next)=>{
+    res.send
+    const post_Id = req.params.postId;
+    const {title, content, tags=""} = req.body;
+    const fields= {}
+    if (title){
+        fields.title= title;
+    }
+    if (content){
+        fields.content= content;
+    }
+    if (tags){
+        if(tags.length>0){
+            fields.tags=tags.trim().split(/\s+/);
+        }
+    }
+
+    try {
+        const originalPost= await getPostById(post_Id);
+        if (originalPost.author.id===req.user.id){
+            const updatedPost=await updatePost(post_Id, fields);
+            res.send({post: updatedPost})
+        } else {
+            next({
+                name: 'UnauthorizedUserError',
+                message: 'You cannot update a post that is not yours'
+            })
+        }
+    } catch ({name, message}) {
+        next({name, message})
+    }
+
 })
 
 postsRouter.get('/', async (req, res) =>{
